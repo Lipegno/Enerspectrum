1b61ebd36393a1020a5bfdce30ced63e63efa703
Jon Sheller <jonsheller@gmail.com>
2015-03-17 16:02:40 +0000

    Remove old files

--- shared/redis-cron/index.js
+++
@@ -1,9 +0,0 @@
-var EventsEmitter = require('events').EventEmitter();
-
-// Schedule recurring event
-exports.schedule = function() {
-};
-
-// Schedule recurring event that will only callback one process's listeners
-exports.scheduleSingle = function() {
-};
